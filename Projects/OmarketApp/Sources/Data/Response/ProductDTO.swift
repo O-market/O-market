10f8c09a3427887563865fd6fb921ae78e3a911a
Lingo <llingo@kakao.com>
2022-08-16 21:53:53 +0900

    feat: ProductDTO 모델 타입 구현 #2

---
+++ Projects/OmarketApp/Sources/Data/Response/ProductDTO.swift
@@ -0,0 +1,36 @@
+//
+//  ProductDTO.swift
+//  OmarketApp
+//
+//  Created by Lingo on 2022/08/16.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import Foundation
+
+struct ProductDTO: Codable {
+  let id: Int
+  let vendorId: Int
+  let name: String
+  let description: String
+  let thumbnail: String
+  let currency: String
+  let price: Double
+  let bargainPrice: Double
+  let discountedPrice: Double
+  let stock: Int
+  var images: [String]?
+  var vendor: String?
+  let createdAt: Date
+  let issuedAt: Date
+
+  private enum CodingKeys: String, CodingKey {
+    case id, name, description, thumbnail, currency, price, stock, images
+    case vendorId = "vendor_id"
+    case bargainPrice = "bargain_price"
+    case discountedPrice = "discounted_price"
+    case vendor = "vendors"
+    case createdAt = "created_at"
+    case issuedAt = "issued_at"
+  }
+}
