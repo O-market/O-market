ab1c4cd737418eedf7e07668866fca148d4083a1
Lingo <llingo@kakao.com>
2022-08-16 22:05:28 +0900

    feat: ProductResponseDTO 모델 타입 구현 #2

---
+++ Projects/OmarketApp/Sources/Data/Response/ProductResponseDTO.swift
@@ -0,0 +1,32 @@
+//
+//  ProductResponseDTO.swift
+//  OmarketApp
+//
+//  Created by Lingo on 2022/08/16.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import Foundation
+
+struct ProductResponseDTO: Codable {
+  let pageNumber: Int
+  let itemsPerPage: Int
+  let totalCount: Int
+  let offset: Int
+  let limit: Int
+  let lastPage: Int
+  let hasNext: Bool
+  let hasPrev: Bool
+  let products: [ProductDTO]
+
+  private enum CodingKeys: String, CodingKey {
+    case offset, limit
+    case pageNumber = "page_no"
+    case itemsPerPage = "items_per_page"
+    case totalCount = "total_count"
+    case lastPage = "last_page"
+    case hasNext = "has_next"
+    case hasPrev = "has_prev"
+    case products = "pages"
+  }
+}
