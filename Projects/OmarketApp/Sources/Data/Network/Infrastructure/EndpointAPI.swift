6f50e880d1f38321ebb1c6346843567fb1608bed
Lingo <llingo@kakao.com>
2022-08-23 21:13:12 +0900

    feat: Endpoint API 타입 구현 #15
    각 서버 요청 Endpoint를 모아둠

--- Projects/OmarketApp/Sources/Data/Network/Infrastructure/EndpointAPI.swift
+++ Projects/OmarketApp/Sources/Data/Network/Infrastructure/EndpointAPI.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+enum EndpointAPI {
+  private enum Base {
+    static let baseURL = "https://market-training.yagom-academy.kr/"
+  }
+
+  case products(ProductRequestQuery)
+  case product(Int)
+
+  var asEndpoint: Endpoint {
+    switch self {
+    case .products(let query):
+      return Endpoint(
+        base: Base.baseURL,
+        path: "/api/products",
+        method: .get,
+        queries: [
+          "page_no": query.pageNumber,
+          "items_per_page": query.itemsPerPage
+        ]
+      )
+
+    case .product(let id):
+      return Endpoint(
+        base: Base.baseURL,
+        path: "/api/products/\(id)",
+        method: .get
+      )
+    }
+  }
+}
