02cc853f5bc7ad18596c23fa1426394858dacb59
Lingo <llingo@kakao.com>
2022-08-23 22:07:42 +0900

    feat: ProductRepositoryTests 파일 구현 #16

---
+++ Projects/OmarketApp/Tests/RepositoryTests/ProductRepositoryTests.swift
@@ -0,0 +1,20 @@
+//
+//  ProductRepositoryTests.swift
+//  OmarketAppTests
+//
+//  Created by Lingo on 2022/08/23.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import XCTest
+@testable import OmarketApp
+
+final class ProductRepositoryTests: XCTestCase {
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+  }
+
+  override func tearDownWithError() throws {
+    try super.tearDownWithError()
+  }
+}
