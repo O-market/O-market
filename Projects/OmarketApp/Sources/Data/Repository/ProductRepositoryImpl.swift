decd30cffbe016aaae3cfcbd06ce07e96a0408ff
Lingo <llingo@kakao.com>
2022-08-23 21:18:59 +0900

    feat: ProductRepositoryImpl 타입 구현 #15
    상품 목록 및 상품에 대한 정보를 가져오는 API 구현
    
    close #15

--- Projects/OmarketApp/Sources/Data/Repository/ProductRepositoryImpl.swift
+++ Projects/OmarketApp/Sources/Data/Repository/ProductRepositoryImpl.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class ProductRepositoryImpl: ProductRepository {
+  private let networkService: NetworkService
+  private let decoder = JSONDecoder()
+
+  init(networkService: NetworkService) {
+    self.networkService = networkService
+  }
+
+  func fetchAllProduct(query: ProductRequestQuery) -> Observable<[Product]> {
+    let endpoint = EndpointAPI.products(query).asEndpoint
+    return networkService.request(endpoint: endpoint)
+      .decode(type: ProductResponseDTO.self, decoder: decoder)
+      .map { $0.products }
+      .map { $0.map { $0.toDomain() } }
+  }
+
+  func fetchProduct(id: Int) -> Observable<Product> {
+    let endpoint = EndpointAPI.product(id).asEndpoint
+    return networkService.request(endpoint: endpoint)
+      .decode(type: ProductDTO.self, decoder: decoder)
+      .map { $0.toDomain() }
+  }
+}
