b29712a283ce0b98944315f3fb17fbf7fae21e3a
saafaaari <111dltldnjs@naver.com>
2022-08-24 19:07:33 +0900

    feat: DetailViewController UI 구현 #11

--- Projects/OmarketApp/Sources/Presentation/MainScene/Detail/ViewController/DetailViewController.swift
+++ Projects/OmarketApp/Sources/Presentation/MainScene/Detail/ViewController/DetailViewController.swift
@@ -7,3 +7,56 @@
 //
 
 import UIKit
+
+import RxSwift
+import SnapKit
+
+final class DetailViewController: UIViewController {
+  let scrollView = UIScrollView()
+  let mainView = DetailView(frame: .zero)
+  let pageViewController = UIPageViewController(transitionStyle: .scroll,
+                                                navigationOrientation: .horizontal)
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+}
+
+// MARK: - UI
+
+extension DetailViewController {
+  private func configureUI() {
+    view.backgroundColor = .systemBackground
+    
+    guard let image = UIImage(systemName: "swift") else { return }
+    let startVC = ProductImageViewController(image: image)
+    
+    pageViewController.setViewControllers([startVC],
+                                          direction: .reverse,
+                                          animated: true)
+    
+    self.addChild(pageViewController)
+    self.view.addSubview(scrollView)
+    
+    scrollView.snp.makeConstraints {
+      $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+    }
+    
+    scrollView.addSubview(pageViewController.view)
+    
+    pageViewController.view.snp.makeConstraints {
+      $0.trailing.leading.top.equalToSuperview()
+      $0.width.equalToSuperview()
+      $0.height.equalToSuperview().multipliedBy(0.55)
+    }
+    
+    scrollView.addSubview(mainView)
+    
+    mainView.snp.makeConstraints {
+      $0.top.equalTo(self.pageViewController.view.snp.bottom).inset(-16)
+      $0.trailing.leading.bottom.equalToSuperview().inset(16)
+    }
+  }
+}
+
