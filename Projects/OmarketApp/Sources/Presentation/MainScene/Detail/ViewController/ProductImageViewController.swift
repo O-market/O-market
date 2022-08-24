d80e7792f06b5df542d080bb695d8374cfc471d1
saafaaari <111dltldnjs@naver.com>
2022-08-24 18:50:34 +0900

    feat: ProductImageViewController 구현 #11

--- Projects/OmarketApp/Sources/Presentation/MainScene/Detail/ViewController/ProductImageViewController.swift
+++ Projects/OmarketApp/Sources/Presentation/MainScene/Detail/ViewController/ProductImageViewController.swift
@@ -7,3 +7,33 @@
 //
 
 import UIKit
+
+final class ProductImageViewController: UIViewController {
+  let imageView: UIImageView
+  
+  init(image: UIImage) {
+    self.imageView = UIImageView(image: image)
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+}
+
+// MARK: - UI
+
+extension ProductImageViewController {
+  
+  private func configureUI() {
+    view.addSubview(imageView)
+    imageView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+}
