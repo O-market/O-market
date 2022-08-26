3ea03686c8d71d943d0bbfa882e50862cd54c8ee
Lingo <llingo@kakao.com>
2022-08-27 00:06:11 +0900

    feat: Storage 프로토콜 타입 생성

---
+++ Projects/Magpie/Sources/Cache/Storage.swift
@@ -0,0 +1,16 @@
+//
+//  Storage.swift
+//  Magpie
+//
+//  Created by Lingo on 2022/08/26.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import Foundation
+
+protocol Storage {
+  associatedtype T
+
+  func setObject(_ object: T, forKey key: String)
+  func value(forKey key: String) -> T?
+}
