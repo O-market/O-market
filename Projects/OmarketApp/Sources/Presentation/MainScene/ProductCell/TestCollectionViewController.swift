8761f67da32749a0edc5d61fd38b085d4a0e02cc
dudu <whqtkf12@naver.com>
2022-08-27 23:01:38 +0900

    style: SwiftLint  오류 수정

--- Projects/OmarketApp/Sources/Presentation/MainScene/ProductCell/TestCollectionViewController.swift
+++ Projects/OmarketApp/Sources/Presentation/MainScene/ProductCell/TestCollectionViewController.swift
@@ -14,8 +14,31 @@ import RxCocoa
 class TestCollectionViewController: UICollectionViewController {
   
   private var dataSource = Observable<[Product]>.of([
-    Product(id: 1, vendorId: 2, name: "맥북프로", thumbnail: "없음", currency: "KRW", price: 9999, bargainPrice: 9000, discountedPrice: 999, stock: 10, createdAt: "2022-01-20T00:00:00.00", issuedAt: "2022-01-20T00:00:00.00"),
-    Product(id: 2, vendorId: 2, name: "맥북에어", thumbnail: "없음", currency: "USD", price: 999, bargainPrice: 999, discountedPrice: 0, stock: 10, createdAt: "2022-01-20T00:00:00.00", issuedAt: "2022-01-20T00:00:00.00")
+    Product(
+      id: 1,
+      vendorId: 2,
+      name: "맥북프로",
+      thumbnail: "없음",
+      currency: "KRW",
+      price: 9999,
+      bargainPrice: 9000,
+      discountedPrice: 999,
+      stock: 10,
+      createdAt: "2022-01-20T00:00:00.00",
+      issuedAt: "2022-01-20T00:00:00.00"
+    ),
+    Product(
+      id: 2,
+      vendorId: 2,
+      name: "맥북에어",
+      thumbnail: "없음",
+      currency: "USD",
+      price: 999,
+      bargainPrice: 999,
+      discountedPrice: 0,
+      stock: 10,
+      createdAt: "2022-01-20T00:00:00.00",
+      issuedAt: "2022-01-20T00:00:00.00")
   ])
   
   private let disposeBag = DisposeBag()
