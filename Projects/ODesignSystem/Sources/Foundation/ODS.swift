b493ac6f58546c2841cff0daac2afd3f3adda6e8
Lingo <llingo@kakao.com>
2022-08-31 22:52:56 +0900

    chore: ODS 및 Font 파일 분리 #30

---
+++ Projects/ODesignSystem/Sources/Foundation/ODS.swift
@@ -0,0 +1,16 @@
+//
+//  ODS.swift
+//  ODesignSystem
+//
+//  Created by Lingo on 2022/08/05.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import UIKit
+
+public enum ODS {
+  public enum Color {}
+  public enum Image {}
+  public enum Icon {}
+  public enum Font {}
+}
---
+++ Projects/ODesignSystem/Sources/Foundation/ODSFont.swift
@@ -0,0 +1,29 @@
+//
+//  ODSFont.swift
+//  ODesignSystem
+//
+//  Created by Lingo on 2022/08/31.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import UIKit
+
+extension ODS.Font {
+  public static let B_R09 = UIFont.systemFont(ofSize: 9.0, weight: .regular)
+  public static let B_R11 = UIFont.systemFont(ofSize: 11.0, weight: .regular)
+  public static let B_M11 = UIFont.systemFont(ofSize: 11.0, weight: .medium)
+  public static let B_B11 = UIFont.systemFont(ofSize: 11.0, weight: .bold)
+  public static let B_R13 = UIFont.systemFont(ofSize: 13.0, weight: .regular)
+  public static let B_B13 = UIFont.systemFont(ofSize: 13.0, weight: .bold)
+  public static let B_R15 = UIFont.systemFont(ofSize: 15.0, weight: .regular)
+  public static let B_M15 = UIFont.systemFont(ofSize: 15.0, weight: .medium)
+  public static let B_B15 = UIFont.systemFont(ofSize: 15.0, weight: .bold)
+  public static let H_R16 = UIFont.systemFont(ofSize: 16.0, weight: .regular)
+  public static let H_M16 = UIFont.systemFont(ofSize: 16.0, weight: .medium)
+  public static let H_B16 = UIFont.systemFont(ofSize: 16.0, weight: .bold)
+  public static let H_M18 = UIFont.systemFont(ofSize: 18.0, weight: .medium)
+  public static let H_B18 = UIFont.systemFont(ofSize: 18.0, weight: .bold)
+  public static let H_B21 = UIFont.systemFont(ofSize: 21.0, weight: .bold)
+  public static let H_B24 = UIFont.systemFont(ofSize: 24.0, weight: .bold)
+  public static let H_B36 = UIFont.systemFont(ofSize: 36.0, weight: .bold)
+}
