e3d89f2eff26d5c15b3135b58f2a4b3dd48a22f7
Lingo <llingo@kakao.com>
2022-08-31 22:53:23 +0900

    chore: Icon, Image 파일 분리 #30

---
+++ Projects/ODesignSystem/Sources/Foundation/ODSImage.swift
@@ -0,0 +1,13 @@
+//
+//  ODSImage.swift
+//  ODesignSystem
+//
+//  Created by Lingo on 2022/08/31.
+//  Copyright © 2022 Omarket. All rights reserved.
+//
+
+import UIKit
+
+extension ODS.Image {
+  public static let logo = UIImage(named: "Logo")
+}
