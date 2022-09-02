b0baf67eb9f7823a26ee767d418c518de96961bd
saafaaari <111dltldnjs@naver.com>
2022-09-02 11:38:59 +0900

    test: DetailViewModel 테스트 케이스 추가 #11

--- Projects/OmarketApp/Tests/ViewModelTests/DetailViewModelTest.swift
+++ Projects/OmarketApp/Tests/ViewModelTests/DetailViewModelTest.swift
@@ -19,6 +19,11 @@ final class DetailViewModelTest: XCTestCase {
   
   override func setUpWithError() throws {
     try super.setUpWithError()
+    let productImage = ProductImage(id: 1,
+                         url: "TestURL",
+                         thumbnailURL: "TestURL",
+                         succeed: true,
+                         issuedAt: "2022-01-24T00:00:00.00")
     dummyProduct = Product(
       id: 1,
       vendorId: 1,
@@ -30,7 +35,7 @@ final class DetailViewModelTest: XCTestCase {
       bargainPrice: 1,
       discountedPrice: 1,
       stock: 1,
-      images: nil,
+      images: [productImage],
       vendor: nil,
       createdAt: "2022/08/23",
       issuedAt: "2022/08/23"
@@ -59,6 +64,30 @@ final class DetailViewModelTest: XCTestCase {
         
         // then
         XCTAssertEqual(product, input)
+      }
+      .disposed(by: disposeBag)
+  }
+  
+  func testProductImageURL을_호출했을_때_dummy와_동일한_URL_배열이_나와야한다() {
+    // given
+    let input = dummyProduct.images!.map { $0.url }
+    // when
+    sut.productImageURL
+      .subscribe { imageURL in
+        // then
+        XCTAssertEqual(imageURL, input)
+      }
+      .disposed(by: disposeBag)
+  }
+  
+  func testProductImageCount을_호출했을_때_dummy의_images개수와_동일한_Count값이_나와야한다() {
+    // given
+    let input = dummyProduct.images!.count
+    // when
+    sut.productImageCount
+      .subscribe { imageCount in
+        // then
+        XCTAssertEqual(imageCount, input)
       }
       .disposed(by: disposeBag)
   }
