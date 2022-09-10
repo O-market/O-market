516a9d8ac56e3fc6e37d697e120043b0748817ef
saafaaari <111dltldnjs@naver.com>
2022-09-11 01:29:19 +0900

    test: ProductFetchUseCaseTests Test Cast 추가 #37

--- Projects/OmarketApp/Tests/UseCaseTests/ProductFetchUseCaseTests.swift
+++ Projects/OmarketApp/Tests/UseCaseTests/ProductFetchUseCaseTests.swift
@@ -14,17 +14,35 @@ import RxSwift
 final class ProductFetchUseCaseTests: XCTestCase {
   private var sut: ProductFetchUseCase!
   private var repository: MockProductRepositoryImpl!
+  private var dummyProduct: Product!
 
   override func setUpWithError() throws {
     try super.setUpWithError()
     repository = MockProductRepositoryImpl()
     sut = ProductFetchUseCaseImpl(repository: repository)
+    dummyProduct = Product(
+      id: 1,
+      vendorId: 1,
+      name: "오픈마켓",
+      description: "오픈마켓입니다",
+      thumbnail: "",
+      currency: "KRW",
+      price: 0,
+      bargainPrice: 0,
+      discountedPrice: 0,
+      stock: 0,
+      images: nil,
+      vendor: nil,
+      createdAt: "2022/08/23",
+      issuedAt: "2022/08/23"
+    )
   }
 
   override func tearDownWithError() throws {
     try super.tearDownWithError()
     repository = nil
     sut = nil
+    dummyProduct = nil
   }
 
   func testFetchAll이_호출되었을때_Repository의_fetchAllProduct_메서드가_호출되어야한다() {
@@ -43,6 +61,15 @@ final class ProductFetchUseCaseTests: XCTestCase {
     _ = sut.fetchOne(id: 1)
     let output = repository.fetchProductCallCount
 
+    // then
+    XCTAssertEqual(output, 1)
+  }
+  
+  func testCreateProduct_호출되었을때_Repository의_createProduct_메서드가_호출되어야한다() {
+    // given when
+    _ = sut.createProduct(product: dummyProduct, images: [Data()])
+    let output = repository.createProductCallCount
+    
     // then
     XCTAssertEqual(output, 1)
   }
