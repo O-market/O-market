0d3d3cf6b77e61444e5a9678e3dcebfd14c6047d
saafaaari <111dltldnjs@naver.com>
2022-10-28 20:57:09 +0900

    feat: CreateVC imageSelect & remove 기능 구현 #47

--- Projects/OmarketApp/Sources/Presentation/MainScene/Create/ViewController/CreateViewController.swift
+++ Projects/OmarketApp/Sources/Presentation/MainScene/Create/ViewController/CreateViewController.swift
@@ -8,19 +8,29 @@
 
 import UIKit
 
+import ODesignSystem
 import RxSwift
 import SnapKit
 
 class CreateViewController: UIViewController {
+  private let viewModel: CreateViewModelable
   private let disposeBag = DisposeBag()
   weak var coordinator: CreateCoordinator?
   private let mainView = CreateView()
+  
+  init(viewModel: CreateViewModelable) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
     bind()
-    
   }
 }
 
@@ -49,7 +59,39 @@ extension CreateViewController {
       .disposed(by: disposeBag)
     
     mainView.photoButton.rx.tap
-      .bind {
+      .bind { [weak self] in
+        guard let self = self else { return }
+        self.coordinator?.presentImagePicker(
+          limitCount: self.viewModel.selectionLimit,
+          delegate: self
+        )
       }.disposed(by: disposeBag)
+    
+    viewModel.numberOfImagesSelected
+      .bind { [weak self] in
+        self?.mainView.photoButton.imageCountLabel.text = "\($0)/10"
+      }
+      .disposed(by: disposeBag)
+  }
+}
+
+extension CreateViewController: MultipleImagePickerDelegate {
+  func picker(picker: UIViewController, results: [ImageManager]) {
+    picker.dismiss(animated: true)
+    results.forEach {
+      $0.request(
+        size: CGSize(width: 100, height: 100)
+      ) { [weak self] image in
+        guard let image = image else { return }
+        guard let pngData = image.pngData() else { return }
+        let imageData = ImageData(data: pngData)
+        let imageView = ImageView(image: image)
+        imageView.removeAction = { [weak self] in
+          self?.viewModel.removeImageData(id: imageData.id)
+        }
+        self?.mainView.addImageView([imageView])
+        self?.viewModel.selectedImageData([imageData])
+      }
+    }
   }
 }
