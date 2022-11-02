b278acf78ea3f7d4d7eda39aca22a5ca6c10abff
saafaaari <111dltldnjs@naver.com>
2022-11-02 21:40:13 +0900

    style: ODSPhotoButtonView 컨벤션 수정

--- Projects/ODesignSystem/Sources/Component/ODSPhotoButtonView.swift
+++ Projects/ODesignSystem/Sources/Component/ODSPhotoButtonView.swift
@@ -13,7 +13,7 @@ import SnapKit
 private class NotHitStackView: UIStackView {
   override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
     let hitView = super.hitTest(point, with: event)
-    if self == hitView { return nil}
+    if self == hitView { return nil }
     return hitView
   }
 }
