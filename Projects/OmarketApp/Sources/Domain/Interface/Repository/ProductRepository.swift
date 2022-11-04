cf576c185cb479468ec7568b39686791f80c7690
saafaaari <111dltldnjs@naver.com>
2022-11-04 20:13:24 +0900

    feat: ProductRepository updataProduct() 구현 #54

--- Projects/OmarketApp/Sources/Domain/Interface/Repository/ProductRepository.swift
+++ Projects/OmarketApp/Sources/Domain/Interface/Repository/ProductRepository.swift
@@ -14,4 +14,5 @@ protocol ProductRepository {
   func fetchAllProduct(endpoint: Endpoint) -> Observable<[Product]>
   func fetchProduct(endpoint: Endpoint) -> Observable<Product>
   func createProduct(endpoint: Endpoint) -> Observable<Void>
+  func updataProduct(endpoint: Endpoint) -> Observable<Void>
 }
