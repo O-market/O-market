d7ede034be5da566beb85a8c35a5a5ac3112b10c
saafaaari <111dltldnjs@naver.com>
2022-11-08 17:00:29 +0900

    feat: EditingVC bind() 구현 및 doneButton 추가 #58

--- Projects/OmarketApp/Sources/Presentation/Writing/ViewController/EditingViewController.swift
+++ Projects/OmarketApp/Sources/Presentation/Writing/ViewController/EditingViewController.swift
@@ -8,24 +8,104 @@
 
 import UIKit
 
+import ODesignSystem
 import RxSwift
 import SnapKit
 
 final class EditingViewController: UIViewController {
-  private let disposeBag = DisposeBag()
+  
+  // MARK: Interfaces
+  
+  private let doneButton = UIBarButtonItem(
+    title: "완료",
+    style: .done,
+    target: nil,
+    action: nil
+  )
   private let mainView = WritingView()
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
+  // MARK: Properties
   
-  init() {
+  private let disposeBag = DisposeBag()
+  private let viewModel: EditingViewModelable
+  
+  // MARK: Life Cycle
+
+  init(viewModel: EditingViewModelable) {
+    self.viewModel = viewModel
     super.init(nibName: nil, bundle: nil)
-    configureUI()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  // MARK: Methods
+  
+  private func setViewItem(_ item: EditingViewModelItem) {
+    mainView.titleTextField.text = item.title
+    mainView.bodyTextView.text = item.body
+    mainView.priceTextField.text = item.price
+    mainView.discountPriceTextField.text = item.discountPrice
+    mainView.stockTextField.text = item.stock
+    
+    item.imageURL.forEach {
+      let imageView = ImageView(imageURL: $0)
+      imageView.removeButton.isHidden = true
+      mainView.addImageView([imageView])
+    }
+  }
+  
+  // MARK: Helpers
+  
+  private func bind(viewModel: EditingViewModelable) {
+    viewModel.viewItem
+      .bind { [weak self] in
+        self?.setViewItem($0)
+      }.disposed(by: disposeBag)
+    
+    doneButton.rx.tap
+      .bind { [weak self] in
+        guard let self = self else { return }
+        viewModel.doneButtonDidTap()
+          .subscribe { _ in
+            self.navigationController?.popViewController(animated: true)
+          } onError: { error in
+            let alert = UIAlertController
+              .makeAlert(message: error.localizedDescription)
+            self.present(alert, animated: true)
+          }.disposed(by: self.disposeBag)
+      }.disposed(by: disposeBag)
+    
+    mainView.titleTextField.rx.text
+      .bind {
+        viewModel.inputTitle($0)
+      }.disposed(by: disposeBag)
+    
+    mainView.bodyTextView.rx.text
+      .bind {
+        viewModel.inputBody($0)
+      }.disposed(by: disposeBag)
+    
+    mainView.priceTextField.rx.text
+      .bind {
+        viewModel.inputPirce($0)
+      }.disposed(by: disposeBag)
+    
+    mainView.discountPriceTextField.rx.text
+      .bind {
+        viewModel.inputDiscountPrice($0)
+      }.disposed(by: disposeBag)
+    
+    mainView.stockTextField.rx.text
+      .bind {
+        viewModel.inputStock($0)
+      }.disposed(by: disposeBag)
   }
 }
 
@@ -33,6 +113,9 @@ final class EditingViewController: UIViewController {
 
 extension EditingViewController {
   private func configureUI() {
+    title = "제품 수정"
+    navigationItem.rightBarButtonItem = doneButton
+    doneButton.tintColor = ODS.Color.brand010
     view.backgroundColor = .systemBackground
     view.addSubview(mainView)
     mainView.photoButton.isHidden = true
