8f0704c3debf9078a7249860d183ee17c36a1cc9
saafaaari <111dltldnjs@naver.com>
2022-11-16 21:20:22 +0900

    chore: 들여쓰기 수정

--- Projects/OmarketApp/Sources/Presentation/Writing/View/WritingView.swift
+++ Projects/OmarketApp/Sources/Presentation/Writing/View/WritingView.swift
@@ -164,11 +164,11 @@ final class WritingView: UIView {
   }
   
   func addImageView(_ view: UIView) {
-      imageStackView.addArrangedSubview(view)
-      view.snp.makeConstraints {
-        $0.width.equalTo(view.snp.height)
-        $0.height.equalToSuperview()
-      }
+    imageStackView.addArrangedSubview(view)
+    view.snp.makeConstraints {
+      $0.width.equalTo(view.snp.height)
+      $0.height.equalToSuperview()
+    }
   }
   
   func checkEmptyTextField() -> String? {
